d71d920c07b08806494c6917c717f81b952bfa69
jojowiththebobo <50336236+jojowiththebobo@users.noreply.github.com>
2019-05-20 11:25:00 -0400

    Add files via upload

--- MainPanel.java
+++ MainPanel.java
@@ -11,13 +11,17 @@ public class MainPanel extends JPanel
    private GamePanel gamepanel;
    private Student student;
    public Scoreboard scoreboard;
+   private String gender = "";
    
    public MainPanel() throws Exception
    {
        setBackground(new Color(64,224,208));
        setLayout(new BorderLayout());
        String name = JOptionPane.showInputDialog("What is your name?");
-       String gender = (JOptionPane.showInputDialog("What is your gender, M or F?"));
+       while(!(gender.equalsIgnoreCase("m") || gender.equalsIgnoreCase("f"))) 
+       {
+         gender = (JOptionPane.showInputDialog("What is your gender, M or F?"));
+       }       
        int type = (int)(Math.random()*3);
        switch(type)
        {
